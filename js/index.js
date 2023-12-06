fd0642400e3308b136fde1e0f4041c83948718ee
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-06 17:12:21 -0500

    add: add movie function

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 "use strict";
 
-(()=>{
-    const movieCon = document.querySelector("#movie-container")
-    async function allMovies(){
-     const response = await  fetch("http://localhost:3000/movies").then(resp => resp.json()).then(data => {return data});
-     renderMovie(response);
-   }
-    console.log(allMovies());
+(() => {
+    const movieCon = document.querySelector("#movie-container");
+    const submitBtn = document.querySelector("#submit-btn");
 
-    function renderMovie(Movies){
-        Movies.forEach(movie => {
+    async function allMovies() {
+        movieCon.innerHTML = `<h2>Loading...</h2>`;
+        const response = await fetch("http://localhost:3000/movies").then(resp => resp.json());
+        movieCon.innerHTML = ``;
+        renderMovie(response);
+    }
+
+    function renderMovie(movies) {
+        movies.forEach(movie => {
             const movieCard = document.createElement("div")
             const movieTitle = document.createElement("h3")
             const movieRating = document.createElement("div")
@@ -21,10 +24,39 @@
             movieCard.appendChild(movieRating);
             movieCard.appendChild(movieSummary);
             movieCon.appendChild(movieCard);
-
-            console.log(movie);
-
-
         })
     }
+
+    async function addMovie(e) {
+        e.preventDefault();
+        const titleVal = document.querySelector("#title").value;
+        const ratingVal = document.querySelector("#rating").value;
+        const newMovieObj = {
+            title: titleVal,
+            rating: ratingVal
+        };
+        try {
+            const url = `http://localhost:3000/movies`;
+            const options = {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newMovieObj)
+            };
+            const resp = await fetch(url, options);
+            const newMovie = await resp.json().then(data => {
+                return data
+            }).catch(error => console.log("error" + error));
+            console.log(newMovie);
+            await allMovies();
+            return newMovie;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    allMovies();
+
+    submitBtn.addEventListener("click", addMovie);
 })();
